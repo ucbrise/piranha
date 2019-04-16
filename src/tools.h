287cf7c05f35e20e4f06c2ac7b0729ebc5c5ffbd
snwagh <snwagh@gmail.com>
2019-04-16 15:46:17 -0400

    With function timing

--- src/tools.h
+++ src/tools.h
@@ -595,5 +595,20 @@ void getVectorfromPrimary(vector<T> &vec, size_t size, string r_mode, string n_m
 	}
 }
 
+#include <chrono>
+#include <utility>
+
+typedef std::chrono::high_resolution_clock::time_point TimeVar;
+
+#define duration(a) std::chrono::duration_cast<std::chrono::milliseconds>(a).count()
+#define timeNow() std::chrono::high_resolution_clock::now()
+
+template<typename F, typename... Args>
+double funcTime(F func, Args&&... args){
+    TimeVar t1=timeNow();
+    func(std::forward<Args>(args)...);
+    return duration(timeNow()-t1);
+}
+
 #endif
 
